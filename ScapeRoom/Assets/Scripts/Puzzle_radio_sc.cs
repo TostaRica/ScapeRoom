a1cf5b7e510e1df6c575d98244a6369cb28c601a
Juan Carlos Sánchez Guzmán <jcarlos0305@hotmail.com>
2021-03-29 16:01:39 +0200

    Added audio clips and source and the play function

--- ScapeRoom/Assets/Scripts/Puzzle_radio_sc.cs
+++ ScapeRoom/Assets/Scripts/Puzzle_radio_sc.cs
@@ -1,43 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Puzzle_radio_sc : Puzzle_sc
 {
-    int result;
-    int[] code;
-    char[] iCode;
-    [SerializeField] private AudioClip a0;
-    [SerializeField] private AudioClip a1;
-    [SerializeField] private AudioClip a2;
-    [SerializeField] private AudioClip a3;
-    [SerializeField] private AudioClip a4;
-    [SerializeField] private AudioClip a5;
-    [SerializeField] private AudioClip a6;
-    [SerializeField] private AudioClip a7;
-    [SerializeField] private AudioClip a8;
-    [SerializeField] private AudioClip a9;
+    private int result;
+    private int[] code;
+    private char[] iCode;
+    //[SerializeField] private AudioClip a0;
+    //[SerializeField] private AudioClip a1;
+    //[SerializeField] private AudioClip a2;
+    //[SerializeField] private AudioClip a3;
+    //[SerializeField] private AudioClip a4;
+    //[SerializeField] private AudioClip a5;
+    //[SerializeField] private AudioClip a6;
+    //[SerializeField] private AudioClip a7;
+    //[SerializeField] private AudioClip a8;
+    //[SerializeField] private AudioClip a9;
+
+    [SerializeField] private AudioClip[] clipList;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        code = new int[]{ 0,1,2,3,4,5,6,7,8,9 };
+        code = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         iCode = new char[code.Length];
         GenerateCode();
+        Activate();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
-    void GenerateCode() {
-        for (int i = code.Length - 1; i > 0; i--) { 
-            int randomIndex = Random.Range(0, i + 1); 
+
+    private void GenerateCode()
+    {
+        for (int i = code.Length - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
             int temp = code[i];
             code[i] = code[randomIndex];
-            code[randomIndex] = temp; 
+            code[randomIndex] = temp;
+
+            AudioClip tempClip = clipList[i];
+            clipList[i] = clipList[randomIndex];
+            clipList[randomIndex] = tempClip;
         }
-       
+
         for (int i = 0; i < iCode.Length; ++i)
         {
             iCode[i] = (char)(code[i] + '0');
@@ -46,19 +56,40 @@ public class Puzzle_radio_sc : Puzzle_sc
         iCode[Random.Range(0, iCode.Length - 1)] = 'X';
     }
 
-    public char[] GetInterfaceCode() {
-
+    public char[] GetInterfaceCode()
+    {
         return iCode;
     }
 
-    public override void Activate() 
+    public override void Activate()
     {
         //activar audio
-        //generar el codigo
+        AudioSource audioSource = GetComponent<AudioSource>();
+        float currentClipLength, timer;
+        timer = 0.0f;
+        currentClipLength = 0.0f;
+
+        for (int i = 0; i < clipList.Length;)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > currentClipLength)
+            {
+                audioSource.Stop();
+
+                audioSource.clip = clipList[i];
+                currentClipLength = clipList[i].length;
+
+                timer = 0;
+
+                audioSource.Play();
+
+                i++;
+            }
+        }
     }
 
     public override void OnResolve()
     {
-
     }
 }
