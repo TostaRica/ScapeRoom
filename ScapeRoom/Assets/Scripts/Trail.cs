cdb1707cc7f380e32a820c32ab24bf69bc015a77
telmiuse <61976005+telmiuse@users.noreply.github.com>
2021-03-30 09:31:20 +0200

    Hearth

--- ScapeRoom/Assets/Scripts/Trail.cs
+++ ScapeRoom/Assets/Scripts/Trail.cs
@@ -4,23 +4,71 @@ using UnityEngine;
 
 public class Trail : MonoBehaviour
 {
+    public Animation animator;
     public float trailSpeed;
+    public float speed;
+    private float timeToReturn = 0.70f;
+    public float currentTime;
+    public GameObject[] shortBeatsPoints;
+    public int wayPoint = 0;
+    public GameObject trail;
     // Start is called before the first frame update
+    public bool beatDone;
+
     void Start()
     {
+        //this.transform.position = GameObject.FindWithTag("LeftWall").transform.position;
     }
 
+    void WaitForReturn()
+    {
+        if (currentTime > 0)
+        {
+            Debug.Log(currentTime);
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            trail.GetComponent<TrailRenderer>().Clear();
+            trail.GetComponent<TrailRenderer>().enabled = true;
+            beatDone = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * Time.deltaTime * trailSpeed);
+        if (beatDone)
+        {
+            WaitForReturn();
+        }
+        else
+        {
+            float step = speed * Time.deltaTime;
+            // calculate distance to move
+            //transform.position = Vector3.MoveTowards(transform.position, shortBeatsPoints[wayPoint].transform.position, step);
+            //transform.Translate(Vector3.right * Time.deltaTime * trailSpeed);
+        }
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("RightWall"))
+        if (other.gameObject.CompareTag("RightWall"))
         {
+            trail.GetComponent<TrailRenderer>().enabled = false;
             this.transform.position = GameObject.FindWithTag("LeftWall").transform.position;
+            wayPoint = 0;
+            beatDone = true;
+            Debug.Log(timeToReturn + "fFff");
+            currentTime = timeToReturn;
+        }
+        if (other.gameObject.CompareTag("LeftWall"))
+        {
+            //trail.GetComponent<TrailRenderer>().Clear();
+            //trail.GetComponent<TrailRenderer>().enabled = true;
+        }
+        if (other.gameObject.CompareTag("Point"))
+        {
+            wayPoint++;
         }
     }
 }
